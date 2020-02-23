03fce937f23bf968eecf49d0f68efc3880ef3b8a
Paul Grokenberger <grokepau@googlemail.com>
2020-02-23 18:20:57 +0200

    added 1 seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+quest = Question.new(content: 'learn how to code')
+quest.save!
+answer1 = ['for free', 1]
+answer2 = ['online', 2]
+answer3 = ['apps', 3]
+answer4 = ['python', 4]
+
+answers = [answer1, answer2, answer3, answer4]
+answers.each do |answer|
+  ansi = Answer.new(rank: answer[1], content: answer[0], question_id: quest)
+  ansi.save!
+end
+
+
