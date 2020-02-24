d61372c009d9578cd37ec0d31c4bc3ba9239ccb5
Sean Keegan <sean@das.llc>
2020-02-24 15:44:10 +0200

    update user w/ has_many games, rounds, and dependent: :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :games, dependent: :destroy
+  has_many :rounds, through: :games
+
+
 end
