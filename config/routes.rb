3d6b86aabe79f8604a9de9e3d82661b3c6bdd4ac
Sean Keegan <sean@das.llc>
2020-02-24 12:08:05 +0200

    changed routes -- remove show from game, e.g.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'pages/menu', to: 'pages#menu', as: :page_menu
-  resources :games, only: %i[create show] do
+  resources :games, only: %i[create] do
     resources :round, only: %i[create]
   end
 
-  get 'games/:id/score', to: 'games#show', as: :game_score
+  get 'games/:id/score', to: 'games#score', as: :game_score
   get 'games/:id/game', to: 'games#show', as: :game_current_round
 end
