48ba189081d5ac83d128332ba8f0cdbe983f0752
Paul Grokenberger <grokepau@googlemail.com>
2020-02-24 16:23:10 +0200

    added first #game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,42 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: [:score, :game]
+
   def create
+    @game = Game.new(game_params)
+    # no user yet (try out)
+    if @game.game_mode == "Try Out"
+      @game.save!
+      redirect_to game_path(@game)
+    end
   end
 
   def score
   end
 
   def game
+    @round = Round.where(game_id: @game.id)
+    if @round[0].nil?
+      @question = rand_quest
+      @answers = Answer.where(question_id: @question.id).shuffle
+
+    else
+
+    end
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:game_mode, :user_id)
+  end
+
+  def rand_quest
+    count = Question.count
+    random_offset = rand(count)
+    return random_quest = Question.offset(random_offset).first
   end
 end
