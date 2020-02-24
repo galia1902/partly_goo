c91cd7ed37e2d4cfc004369b04b76afd05d717f6
Sean Keegan <sean@das.llc>
2020-02-24 16:37:35 +0200

    Added Commented-out policy options to Round, and default to allow create 'Try Out'

--- app/policies/round_policy.rb
+++ app/policies/round_policy.rb
@@ -6,7 +6,13 @@ class RoundPolicy < ApplicationPolicy
   end
 
   def create
+    # Can only create a round if mode == tryout || user = owner of current game
+    record.game.mode == 'Try Out' || user == record.game.user
+
+    # Anyone can create a round
+    # true
+
     # only the owner of the current game can create a round
-    record.game.user == user
+    # record.game.user == user
   end
 end
