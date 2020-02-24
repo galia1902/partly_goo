7f4f1e398981420e027f65a2edcb0c3e1dc26e61
Sean Keegan <sean@das.llc>
2020-02-24 11:54:13 +0200

    Added skeleton routes --> 3 routes all to games#show?

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'pages/menu', to: 'pages#menu', as: :page_menu
+  resources :games, only: %i[create show] do
+    resources :round, only: %i[create]
+  end
+
+  get 'games/:id/score', to: 'games#show', as: :game_score
+  get 'games/:id/game', to: 'games#show', as: :game_current_round
 end
