9d16ecdb8cdd2bbe81b42cb8482affb752d2a9e4
Paul Grokenberger <grokepau@googlemail.com>
2020-02-25 16:33:42 +0200

    first part of htp done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :how_to]
 
   def home
   end
 
   def menu
   end
+
+  def how_to
+  end
+
 end
