724e760ab3bfbe52cd68b36f5cd2b3da2d06b20a
Paul Grokenberger <grokepau@googlemail.com>
2020-03-02 13:29:02 +0200

    in mid of sortable view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,5 @@
 import "bootstrap";
 import { initSortable } from '../plugins/init_sortable';
+
+
+initSortable();
--- app/javascript/plugins/init_sortable.js
+++ app/javascript/plugins/init_sortable.js
@@ -1,8 +1,17 @@
-import Sortable from 'sortablejs';
+import Sortable from 'sortablejs/modular/sortable.complete.esm.js';
 
 const initSortable = () => {
+
   const list = document.querySelector('#quest-sort');
-  Sortable.create(list);
+  // Sortable.create(list);
+  Sortable.create(list, {
+    ghostClass: "ghost",
+    animation: 150,
+    swap: true,
+    swapClass: 'highlight',
+    dragClass: "sortable-drag",
+  });
+
 };
 
 export { initSortable };
